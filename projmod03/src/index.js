389e8b4de8dc4c675cdf9bd644527ce26d58c484
Tyfani21 <tyfani.abreu@gmail.com>
2021-09-07 16:56:01 -0400

    git
    Signed-off-by: Tyfani21 <tyfani.abreu@gmail.com>

--- projmod03/src/index.js
+++ projmod03/src/index.js
@@ -10,8 +10,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-console.log("ola mundo")
-console.log("teste")
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
