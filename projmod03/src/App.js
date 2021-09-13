85a736654cf716f2d6e263bc2317f55f2180ca05
Tyfani21 <tyfani.abreu@gmail.com>
2021-09-13 18:30:15 -0400

    REACT
    Signed-off-by: Tyfani21 <tyfani.abreu@gmail.com>

--- projmod03/src/App.js
+++ projmod03/src/App.js
@@ -2,15 +2,18 @@ import './App.scss';
 import { Switch, Route} from 'react-router-dom';
 import Home from './pages/home/home';
 import Addtaref from './pages/addtaref/Addtaref';
-
+import Header from './components/shared/header/header'
+import Footer from './components/shared/footer/footer'
 
 function App() {
   return (
     <div className="App">  
+      <Header/>
       <Switch>
       <Route path="/" exact={true} component={Home}></Route>
       <Route path="/add" component={Addtaref}></Route>
       </Switch>
+      <Footer/>
     </div>
   );
 }
--- projmod03/src/pages/addtaref/Addtaref.js
+++ projmod03/src/pages/addtaref/Addtaref.js
@@ -1,13 +1,25 @@
 import './addtaref.scss'
 import React from 'react'
 
+
 const NovaTarefa = () => {
-    return (<div>
+    return (<section className='add'>
 
         <h1>Adicione uma nova Tarefa</h1>
-        <input type='text' placeholder='Adicione uma nova tarefa'></input>
-
-    </div>)
+    <div className='group-form'>
+        <form className='add-form' /*onSubmit={handleSubmit}*/>
+        <label htmlFor='descricao' className='add-form-group-label'>Descrição da Tarefa</label>
+        <textarea type='text' id='describe' name='describe' className='add-form-group-area'></textarea>
+        <label htmlFor='data' className='add-form-group-label'>Data da Tarefa:</label>
+        <input type='date'></input>
+        <label htmlFor='hora' className='add-form-group-label'>Horário</label>
+        <input type='time'></input>
+        <label htmlFor='lembrete' className='add-form-group-label'>Adicionar um lembrente?</label>
+        <input type='checkbox'></input>
+        <button type='submit'>Adicionar nova tarefa</button>
+    </form>
+    </div>
+    </section>)
 
 }
 
